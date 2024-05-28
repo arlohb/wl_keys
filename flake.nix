73d7d4116e2b43c32b6d5a621101fb99a5154d2a
arlohb <arlo@urwick.co.uk>
2024-05-28 18:26:35 +0100

    Added build to flake

--- flake.nix
+++ flake.nix
@@ -7,6 +7,21 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
+
+        name = "wl_keys";
+        version = "0.1.0";
+        deps = with pkgs; [
+          protobuf
+        ];
+
+        package = pkgs.rustPlatform.buildRustPackage {
+          inherit version;
+          pname = name;
+
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = deps;
+        };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
@@ -15,9 +30,12 @@
             rust-analyzer
             clippy
             rustfmt
+          ] ++ deps;
+        };
 
-            protobuf
-          ];
+        packages = rec {
+          "${name}" = package;
+          default = package;
         };
       }
     );
